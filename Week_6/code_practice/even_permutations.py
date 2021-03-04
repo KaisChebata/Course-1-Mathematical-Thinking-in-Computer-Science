8f32a24b3b533a33c29a39dd7211daef631801d6
Kais Chebata <qays.sh@gmail.com>
2021-03-04 19:28:49 +0100

    add is_even_permutation function that check if permutation is even or not

--- Week_6/code_practice/even_permutations.py
+++ Week_6/code_practice/even_permutations.py
@@ -26,3 +26,18 @@ def is_permutation(permutaion):
 print (is_permutation([0]))
 print (is_permutation([0,2,1]))
 print (is_permutation([1,2,3]))
+
+def is_even_permutation(permutaion):
+    if is_permutation(permutaion):
+        even_counter = 0
+        for i in range(0, len(permutaion)):
+            for j in range(i + 1, len(permutaion)):
+                if permutaion[i] > permutaion[j]:
+                    even_counter += 1
+        
+        return True if even_counter % 2 == 0 else False
+    
+    return False
+
+print(is_even_permutation([4, 3, 2, 1]))
+print(is_even_permutation([0,3,2,4,5,6,7,1,9,8]))
